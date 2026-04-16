cac102ccd78bc8554f49cc1906f0f06e85290378
Isabel Morales <isabeldelrociovmorales@gmail.com>
2026-04-16 16:30:17 -0700

    Initial Commit

---
+++ 2026-04-26-IntroGitHubTest-Isabel/_026_04_26_IntroGitHubTest_IsabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  _026_04_26_IntroGitHubTest_IsabelApp.swift
+//  2026-04-26-IntroGitHubTest-Isabel
+//
+//  Created by isabel on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _026_04_26_IntroGitHubTest_IsabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
